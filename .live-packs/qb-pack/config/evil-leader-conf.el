d7eb723b3d4e642e6450d20d367e67641636598b
Szczyp <qb@szczyp.com>
2013-04-24 00:09:11 +0200

    Add qb-pack for emacs-live

---
+++ .live-packs/qb-pack/config/evil-leader-conf.el
@@ -0,0 +1,8 @@
+(evil-leader/set-key
+  "x" 'smex
+  "b" 'ido-switch-buffer
+  "k" 'ido-kill-buffer
+  "f" 'ido-find-file
+  "t" 'find-file-in-project
+  "d" 'ac-nrepl-popup-doc
+  "g" 'nrepl-jump)
---
+++ .live-packs/qb-pack/init.el
@@ -0,0 +1,10 @@
+;; User pack init file
+;;
+;; User this file to initiate the pack configuration.
+;; See README for more information.
+
+;; Load bindings config
+(live-load-config-file "bindings.el")
+(live-load-config-file "ac-nrepl-conf.el")
+(live-load-config-file "find-file-in-project-conf.el")
+(live-load-config-file "evil-leader-conf.el")
