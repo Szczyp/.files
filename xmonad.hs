caafd6cdf9ebdd5c2250a566d2eaf90bba03743c
Szczyp <qb@szczyp.com>
2014-09-24 18:08:23 +0200

    Improve xmonad layout

--- xmonad.hs
+++ xmonad.hs
@@ -1,11 +1,14 @@
-import Data.Monoid
 import XMonad
 import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.SetWMName
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.NoBorders
 import XMonad.Util.EZConfig (additionalKeys)
 
+layout = tiled ||| Full
+  where tiled = Tall nmaster delta ratio
+        nmaster = 1
+        ratio = 1/2
+        delta = 1/8
 
 main :: IO ()
 main = do
@@ -14,6 +17,6 @@ main = do
   xmonad $ defaultConfig
     { manageHook      = manageDocks <+> fullscreenManageHook <+> manageHook defaultConfig
     , handleEventHook = fullscreenEventHook
-    , layoutHook      = noBorders . fullscreenFull . avoidStruts $ layoutHook defaultConfig
+    , layoutHook      = noBorders . fullscreenFull . avoidStruts $ layout
     , modMask         = mod4Mask
     } `additionalKeys` [ ((mod4Mask, xK_p), spawn "dmenu_run -i") ]
